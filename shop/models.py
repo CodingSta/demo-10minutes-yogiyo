6a8b1b3b3dcc4fbae672ed6db376480f7f6d91d2
Chinseok Lee <me@nomade.kr>
2018-02-13 15:48:10 +0900

    shop 모델 추가 및 마이그레이션

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,38 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    icon = models.ImageField(blank=True)
+    is_public = models.BooleanField(default=False, db_index=True)
+
+
+class Shop(models.Model):
+    category = models.ForeignKey(Category)
+    name = models.CharField(max_length=100, db_index=True)
+    desc = models.TextField(blank=True)
+    photo = models.ImageField(blank=True)
+    is_public = models.BooleanField(default=False, db_index=True)
+
+
+class Review(models.Model):
+    shop = models.ForeignKey(Shop)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    photo = models.ImageField(blank=True)
+    rating = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    message = models.TextField()
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    desc = models.TextField(blank=True)
+    amount = models.PositiveIntegerField()
+    photo = models.ImageField(blank=True)
+    is_public = models.BooleanField(default=False, db_index=True)
+
+
+# class Order(models.Model):
+#    pass
+
