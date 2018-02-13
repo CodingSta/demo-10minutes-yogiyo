a6b2feea0cee62d7c396552175bb13903dd91912
Chinseok Lee <me@nomade.kr>
2018-02-13 15:37:41 +0900

    initial commit

---
+++ baemin/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^shop/', include('shop.urls', namespace='shop')),
+]
+
